bb591cf6d9c6d80e415e92e09192b1d9a801c2f6
Diane <balthazaa@gmail.com>
2026-06-08 11:02:35 +0800

    feat: API 迁移到 wallsquat 子域名 + next.config 添加 basePath

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 静态导出，适配 Cloudflare Pages
+  basePath: '/wallsquat',
+  trailingSlash: true,
   output: 'export',
-  basePath: '',
-  assetPrefix: '',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  distDir: 'out',
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
